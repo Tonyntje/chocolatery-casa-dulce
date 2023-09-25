63b884bc621b0700636518e45c8c702d01f8be6a
Tony Nieuwenhuijs <tony_nieuwenhuijs@live.nl>
2023-09-25 23:49:42 +0200

    Installed packages Bun + Astro + React + Tailwind

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="w-full p6 text-xl flex space-between">
+            Text
+        </div>
+    )
+}
+
+export default Header
